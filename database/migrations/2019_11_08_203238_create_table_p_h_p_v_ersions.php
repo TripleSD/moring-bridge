c26167121bce3fba86c27c6227da907bf4b25400
Anton Zheltyshev <az@maxrival.com>
2019-11-09 01:44:51 +0300

    Create table for phpversion.

---
+++ database/migrations/2019_11_08_203238_create_table_p_h_p_v_ersions.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablePHPVErsions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('php_versions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('version')->comment('Last version number');
+            $table->integer('branch')->comment('Branch');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('php_versions');
+    }
+}
