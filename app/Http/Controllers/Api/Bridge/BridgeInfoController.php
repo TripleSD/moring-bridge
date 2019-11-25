be4a65358703f1895c8d12fda879e04791c380c2
Anton Zheltyshev <az@maxrival.com>
2019-11-25 22:25:09 +0300

    Added checking identificator.

--- app/Http/Controllers/Api/Bridge/BridgeInfoController.php
+++ app/Http/Controllers/Api/Bridge/BridgeInfoController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers\Api\Bridge;
 
 use App\Http\Controllers\Controller;
+use App\Models\StatisticsIdentificators;
 use App\Repositories\BridgeInfoRepository;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
 
 class BridgeInfoController extends Controller
 {
-    public function getCurrentVersion(BridgeInfoRepository $repository)
+    public function getCurrentVersion(Request $request, BridgeInfoRepository $repository)
     {
-        return Response::json($repository->getCurrentVersion(), 200,
-            array('Content-Type' => 'application/json;charset=utf8'), JSON_UNESCAPED_SLASHES);
+        $identificators = StatisticsIdentificators::where('identificator', $request->identificator)->first();
+
+        if ($identificators != null) {
+            return Response::json($repository->getCurrentVersion(), 200,
+                array('Content-Type' => 'application/json;charset=utf8'), JSON_UNESCAPED_SLASHES);
+        } else {
+            return Response::json(null, 200,
+                array('Content-Type' => 'application/json;charset=utf8'), JSON_UNESCAPED_SLASHES);
+        }
     }
 }
