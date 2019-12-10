52bd4ade6e914ca2b028116d17aa40f28d4f5c5f
Anton Zheltyshev <az@maxrival.com>
2019-12-10 13:47:32 +0300

    PSR12.

--- app/Http/Controllers/Api/Bridge/BridgeInfoController.php
+++ app/Http/Controllers/Api/Bridge/BridgeInfoController.php
@@ -16,11 +16,19 @@ class BridgeInfoController extends Controller
         $identificators = StatisticsIdentificators::where('identificator', $request->identificator)->first();
 
         if ($identificators != null) {
-            return Response::json($repository->getCurrentVersion(), 200,
-                array('Content-Type' => 'application/json;charset=utf8'), JSON_UNESCAPED_SLASHES);
+            return Response::json(
+                $repository->getCurrentVersion(),
+                200,
+                array('Content-Type' => 'application/json;charset=utf8'),
+                JSON_UNESCAPED_SLASHES
+            );
         } else {
-            return Response::json(null, 200,
-                array('Content-Type' => 'application/json;charset=utf8'), JSON_UNESCAPED_SLASHES);
+            return Response::json(
+                null,
+                200,
+                array('Content-Type' => 'application/json;charset=utf8'),
+                JSON_UNESCAPED_SLASHES
+            );
         }
     }
 }
