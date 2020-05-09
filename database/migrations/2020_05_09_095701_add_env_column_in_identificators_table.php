924a9c27f9c0812c05731097243b5e76057fd68d
Anton Zheltyshev <az@maxrival.com>
2020-05-09 12:59:14 +0300

    Added env column.

---
+++ database/migrations/2020_05_09_095701_add_env_column_in_identificators_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEnvColumnInIdentificatorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+            'identificators',
+            function (Blueprint $table) {
+                $table->string('env')->after('remote_addr')->comment('Client version Env');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('identificators', function (Blueprint $table) {
+            //
+        });
+    }
+}
