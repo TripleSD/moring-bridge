bba29503127a6de7c1e39ee1183747cfbc610438
Anton Zheltyshev <az@maxrival.com>
2020-05-09 13:05:40 +0300

    Added env for identificator's check.

--- app/Repositories/IdentificatorsRepository.php
+++ app/Repositories/IdentificatorsRepository.php
@@ -14,27 +14,29 @@ class IdentificatorsRepository extends Repository
      */
     public function createIdentificator($request)
     {
-        $uuid = Uuid::uuid1();
-        $identificator = new Identificators();
+        $uuid                         = Uuid::uuid1();
+        $identificator                = new Identificators();
         $identificator->identificator = $uuid;
-        $identificator->remote_addr = ip2long($request->server('REMOTE_ADDR'));
+        $identificator->remote_addr   = ip2long($request->server('REMOTE_ADDR'));
         $identificator->save();
-        return (string)$identificator->getAttributeValue('identificator');
+        return (string) $identificator->getAttributeValue('identificator');
     }
 
     /*
      * Проверка идентификатора
      * Identificator's check
      */
-    public function checkIdentificator($identificator)
+    public function checkIdentificator($identificator, $env)
     {
         $identificator = Identificators::where('identificator', $identificator)
+            ->where('env', $env)
             ->pluck('identificator')->first();
 
         if ($identificator != null) {
-            Identificators::where('identificator', $identificator)->update(['updated_at' => Carbon::now()]);
+            Identificators::where('identificator', $identificator)
+                ->update(['env' => $env, 'updated_at' => Carbon::now()]);
         }
 
-        return (string)$identificator;
+        return (string) $identificator;
     }
 }
