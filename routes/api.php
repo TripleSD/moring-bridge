5c23cd71530fe890930829922e121463142ab73c
Anton Zheltyshev <az@maxrival.com>
2020-05-11 13:59:46 +0300

    Added route for version statistics.

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,12 @@ Route::group(['prefix' => 'v1', 'namespace' => 'Api'], function () {
         Route::get('versions', 'PhpInfoController@getActualVersions');
     });
 
-//    Route::group(['prefix' => 'statistics','namespace' => 'Statistics'], function () {
+    Route::group(['prefix' => 'statistics','namespace' => 'Statistics'], function () {
+        Route::post('/versions/update', 'StatController@updateVersion');
 //        Route::post('host', 'StatisticsSaveController@postHost');
 //        Route::post('servers', 'StatisticsSaveController@postServers');
 //        Route::post('sites', 'StatisticsSaveController@postSites');
-//    });
+    });
 
     Route::group(['prefix' => 'identificators','namespace' => 'Identificators'],function() {
         Route::get('/create', 'IdentificatorsInfoController@createIdentificator');
