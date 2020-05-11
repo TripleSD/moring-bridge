126c897d09a00a4352a06d3f07d1ffefa8fbce3a
Anton Zheltyshev <az@maxrival.com>
2020-05-11 14:00:50 +0300

    Added controller & method for version statistics.

---
+++ app/Http/Controllers/Api/Stat/StatController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\Php;
+
+use Carbon\Carbon;
+use App\Models\Identificators;
+use App\Models\StatisticsVersions;
+use App\Http\Controllers\Controller;
+use App\Repositories\IdentificatorsRepository;
+use App\Repositories\PHPVersionsRepository;
+use Illuminate\Http\Request;
+
+class StatController extends Controller
+{
+    private $identificatorsRepository;
+
+    public function __construct()
+    {
+        $this->identificatorsRepository = new IdentificatorsRepository();
+    }
+
+    public function updateVersion(Request $request, PHPVersionsRepository $repository)
+    {
+        $currentVersion = StatisticsVersions::where('identicator', $request->input('identificator'))
+            ->where('version', $request->input('version'))->get();
+
+        if (empty($currentVersion)) {
+            StatisticsVersions::store(
+                ['identificator' => $request->input('identificator'), 'version' => Carbon::now()]
+            );
+        }
+    }
+}
